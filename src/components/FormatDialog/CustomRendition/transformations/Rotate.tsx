1739898caa56db511ad3202d5d5decb11deb5d8d
Khoa Le <khoa.le@orangelogic.com>
2025-03-28 10:27:52 +0000

    Merged PR 52352: 41M8IP - fix: allow empty string in Rotation input
    fix: allow empty string in Rotation input
    
    https://link.orangelogic.com/Tasks/41M8IP

--- src/components/FormatDialog/CustomRendition/transformations/Rotate.tsx
+++ src/components/FormatDialog/CustomRendition/transformations/Rotate.tsx
@@ -15,13 +15,14 @@ const sanitizeRotation = (rotation: number) => {
   if (rotation >= 360) {
     return 359;
   } else if (rotation < 0) {
-    return 360 + rotation;
+    return 0;
   }
   return rotation;
 };
 
 const Rotate = ({ open, rotation, onChange, onApply }: Props) => {
   const [isDefined, setIsDefined] = useState(false);
+  const [invalidRotation, setInvalidRotation] = useState(false);
 
   useEffect(() => {
     Promise.all([
@@ -32,8 +33,21 @@ const Rotate = ({ open, rotation, onChange, onApply }: Props) => {
   }, []);
 
   const handleRotationChange = _debounce((e: FormEvent<CxInput>) => {
-    const newRotation = Number((e.target as HTMLInputElement).value);
-    onChange(sanitizeRotation(newRotation));
+    const value = (e.target as HTMLInputElement).value;
+    const newRotation = Number(value);
+    if (!value) {
+      setInvalidRotation(true);
+    } else {
+      setInvalidRotation(false);
+    }
+
+    // When the user change the value to a number >= 360 for the second time in a row, 
+    // The input won't update the value back to 359, so we need to do it manually
+    const sanitizedRotation = sanitizeRotation(newRotation);
+    if (newRotation >= 360 || newRotation < 0) {
+      (e.target as HTMLInputElement).value = sanitizedRotation.toString();
+    }
+    onChange(sanitizedRotation);
   }, INPUT_DEBOUNCE_DELAY);
 
   return (
@@ -60,7 +74,7 @@ const Rotate = ({ open, rotation, onChange, onApply }: Props) => {
           </cx-button>
         </cx-button-group>
         <cx-input
-          value={isDefined ? rotation.toString() : ''}
+          value={isDefined && !invalidRotation ? rotation.toString() : ''}
           placeholder="0"
           type="number"
           style={{
@@ -72,7 +86,7 @@ const Rotate = ({ open, rotation, onChange, onApply }: Props) => {
         ></cx-input>
         <cx-button
           variant="primary"
-          disabled={rotation === 0}
+          disabled={rotation === 0 || invalidRotation}
           onClick={onApply}
         >
           Apply
