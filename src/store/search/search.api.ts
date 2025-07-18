d6fd6dddd439ab3ae2ad03dfd3c1cb45d9543a0f
Tam Tran <tam.tran@orangelogic.com>
2025-07-18 07:35:41 +0000

    Merged PR 60424: fix: add 'SeeThru' parameter to search API request for enhanced filtering
    fix: add 'SeeThru' parameter to search API request for enhanced filtering
    (https://link.orangelogic.com/Tasks/41QIO6)

--- src/store/search/search.api.ts
+++ src/store/search/search.api.ts
@@ -92,6 +92,7 @@ export const searchApi = createApi({
 
         if (searchText) {
           params.push(['Text', searchText]);
+          params.push(['SeeThru', 'true']);
         }
 
         if (pageSize) {
